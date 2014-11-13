a45a983cc5c24ac4266cc21114d1eea1732ee572
vyadav22 <vikas.yadav@student.dit.ie>
2014-11-13 15:44:39 +0000

    queued up myself

--- list-was-queue.py
+++ list-was-queue.py
@@ -9,7 +9,10 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
+
 conn = boto.sqs.connect_to_region("eu-east-1", aws_access_key_id='KIAIR7EH3TNSTDUCWKA', aws_secret_access_key='t2FZT5mrLYy8gX7kS1q0p4ObQYXTwGnaiUm+rxHZ')
+
+q = conn.crete_queue("C12386676",120)
 
 rs = conn.get_all_queues()
 for q in rs:
