2330cb8e6bb2c8c39d5b54c0fbc144d6eaab0f6f
vyadav22 <vikas.yadav@student.dit.ie>
2014-11-13 15:33:47 +0000

    key changed

--- list-was-queue.py
+++ list-was-queue.py
@@ -9,7 +9,7 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region("eu-east-1", aws_access_key_id='KIAIR7EH3TNSTDUCWKA', aws_secret_access_key='t2FZT5mrLYy8gX7kS1q0p4ObQYXTwGnaiUm+rxHZ')
 
 rs = conn.get_all_queues()
 for q in rs:
